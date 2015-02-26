53b638f8ae5a7bf9cb945d1c2aec3f0e650a3854
binaymishra <binaymishramca@gmail.com>
2015-02-26 09:03:01 +0530

    fibonacci series

--- apptest/src/apptest/hof.clj
+++ apptest/src/apptest/hof.clj
@@ -1,3 +1,4 @@
+;;Author Binay Mishra
 (ns apptest.hof)
 
 (defn hof-test
@@ -10,3 +11,19 @@
   (if (zero? n)
     1
     ( * (reduce * (range 1N n)) n)))
+
+;;Conbination [eg. nCr ] 
+(defn combination
+  [n r]
+  (if (>= n r)
+      (/ (factorial n) (* (factorial r) (factorial (- n r ))))))
+
+;;Fibonacci Term. [e g. (fibonacci-term [0 1]) ]
+(defn fibonacci-term
+  [ [a b] ] 
+  [b (+' a b )])
+
+;;Fibonacci Series [e.g fibonacci series up to 5 is (fibonacci-series 5)]
+(defn fibonacci-series
+  [n]
+  (take n (map first (iterate fibonacci-term [0 1]))))
